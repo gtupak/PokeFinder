f30960e07d166269aea9c6da2f5722262c545b39
Gabriel T <gabriel.tapuc@gmail.com>
2017-10-02 10:03:31 -0400

    Initial commit.

---
+++ PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Gabriel T on 2017-10-02.
+//  Copyright © 2017 Gabriel T. All rights reserved.
+//
+
+#ifndef PokeFinder_Bridging_Header_h
+#define PokeFinder_Bridging_Header_h
+
+
+#endif /* PokeFinder_Bridging_Header_h */
+
+#import "Geofire.h"
